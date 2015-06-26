d655e0b5eaa25b3ad054de61a261fb7d4db3b098
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-06-26 14:39:07 -0400

    I had to use the way Matt did to get my api working properly because I had created a mightmare with properties from my getGeoloaction

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(params) {
+		return Ember.$.ajax("https://api.forecast.io/forecast/21864a8f61715df6c3752a4464449bae/"+params.lat+","+params.lng,
+		{dataType: 'jsonp'}).then(function (response) {
+			return response;
+			});
+	}
 });
